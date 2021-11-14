b0a18b9b467c1f9cee996d683c8a5537c183c8d5
Alexander <alexneksikan@rambler.ru>
2021-11-14 15:29:51 +0300

    рок 8. Полезное: страничный вывод, шаблонные фильтры, CBV

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,6 +1,6 @@
 from authapp.forms import ShopUserEditForm
 from authapp.models import ShopUser
-from mainapp.models import ProductCategory
+from mainapp.models import Product, ProductCategory
 from django import forms
 
 
@@ -10,10 +10,24 @@ class ShopUserAdminEditForm(ShopUserEditForm):
         fields = '__all__'
 
 
+class ProductEditForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        # fields = '__all__'
+        exclude = ('is_active',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
 class ProductCategoryEditForm(forms.ModelForm):
     class Meta:
         model = ProductCategory
-        fields = '__all__'
+        exclude = ('is_active',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
