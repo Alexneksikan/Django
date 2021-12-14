50a19a797125f9f1ada05634d6cbddd7327c7c06
Alexander <alexneksikan@rambler.ru>
2021-12-15 00:26:04 +0300

    low_cache

--- mainapp/views.py
+++ mainapp/views.py
@@ -19,8 +19,67 @@ def get_links_menu():
     return ProductCategory.objects.filter(is_active=True)
 
 
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category_item = cache.get(key)
+        if category_item is None:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category_item)
+        return category_item
+    return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_products_ordered_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_ordered_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+
+
+def get_products_in_category_ordered_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_ordered_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+
+
 def get_hot_product():
-    return random.sample(list(Product.objects.all()), 1)[0]
+    return random.sample(list(get_product()), 1)[0]
 
 
 def get_same_product(hot_product):
@@ -32,7 +91,7 @@ def get_same_product(hot_product):
 def index(request):
     context = {
         'title': 'Главная',
-        'products': Product.objects.all()[:4]
+        'products': get_products()[:4]
 
     }
     return render(request, 'mainapp/index.html', context)
@@ -50,14 +109,14 @@ def products(request, pk=None, page=1):
     links_menu = get_links_menu()
     if pk is not None:
         if pk == 0:
-            products_list = Product.objects.all()
+            products_list = get_products_ordered_by_price()
             category_item = {
                 'name': 'все',
                 'pk': 0
             }
         else:
-            category_item = get_object_or_404(ProductCategory, pk=pk)
-            products_list = Product.objects.filter(category__pk=pk)
+            category_item = get_category(pk)
+            products_list = get_products_in_category_ordered_by_price(pk)
 
         paginator = Paginator(products_list, 2)
         try:
@@ -88,7 +147,7 @@ def products(request, pk=None, page=1):
 def product(request, pk):
     links_menu = get_links_menu()
     context = {
-        'product': get_object_or_404(Product, pk=pk),
+        'product': get_product(pk),
         'links_menu': links_menu
     }
 
